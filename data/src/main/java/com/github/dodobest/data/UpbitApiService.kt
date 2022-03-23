0b91cc11c34b76f2826f6035d8469ae418305fdb
DoDoBest <av449@naver.com>
2022-03-23 20:01:47 +0900

    chore: 사용하지 않는 import문 삭제

--- data/src/main/java/com/github/dodobest/data/UpbitApiService.kt
+++ data/src/main/java/com/github/dodobest/data/UpbitApiService.kt
@@ -2,7 +2,6 @@ package com.github.dodobest.data
 
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface UpbitApiService {
