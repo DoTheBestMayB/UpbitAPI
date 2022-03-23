52367dc34bedda60fc7c980956a8637a7923e598
DoDoBest <av449@naver.com>
2022-03-23 15:45:53 +0900

    feat: viewModelFactory 생성

---
+++ app/src/main/java/com/github/dodobest/upbitapi/GithubViewModelFactory.kt
@@ -0,0 +1,21 @@
+package com.github.dodobest.upbitapi
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.github.dodobest.data.Injector
+
+class GithubViewModelFactory : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return when (modelClass) {
+            UpbitApiViewModel::class.java -> createUpbitApiViewModel()
+            else -> throw IllegalArgumentException()
+        } as T
+    }
+
+    private fun createUpbitApiViewModel(): UpbitApiViewModel {
+        val retrofit = Injector.provideRetrofit()
+        val upbitRepository = Injector.provideUpbitRepository(retrofit)
+        return UpbitApiViewModel(upbitRepository)
+    }
+}
