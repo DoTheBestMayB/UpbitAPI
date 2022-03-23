2d8b627cff6ab5cb042e9d1c6421c1a5f343470d
DoDoBest <av449@naver.com>
2022-03-23 15:50:38 +0900

    feat: MainActivity 바인딩

--- app/src/main/java/com/github/dodobest/upbitapi/MainActivity.kt
+++ app/src/main/java/com/github/dodobest/upbitapi/MainActivity.kt
@@ -2,10 +2,19 @@ package com.github.dodobest.upbitapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.github.dodobest.upbitapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: UpbitApiViewModel by viewModels { GithubViewModelFactory() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
     }
 }
