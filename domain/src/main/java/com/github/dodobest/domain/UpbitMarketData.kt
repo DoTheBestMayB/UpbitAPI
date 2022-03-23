19a0096c66b00c940453ec1147b18147af6766e9
DoDoBest <av449@naver.com>
2022-03-23 14:42:15 +0900

    feat: domain 모듈에 수신 데이터 클래스 생성

---
+++ domain/src/main/java/com/github/dodobest/domain/UpbitMarketData.kt
@@ -0,0 +1,8 @@
+package com.github.dodobest.domain
+
+data class UpbitMarketData(
+    val market: String,
+    val koreanName: String,
+    val englishName: String,
+    val marketWarning: String
+)
