f53358dacca8e460e46c3bf9cbb04be7a902aa1b
DoDoBest <av449@naver.com>
2022-03-23 16:10:05 +0900

    fix: retrofit에 rxjava3 adapter 설정

--- data/src/main/java/com/github/dodobest/data/Injector.kt
+++ data/src/main/java/com/github/dodobest/data/Injector.kt
@@ -1,6 +1,7 @@
 package com.github.dodobest.data
 
 import com.github.dodobest.domain.UpbitRepository
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -10,6 +11,7 @@ object Injector {
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(API.BASE_URL)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .client(OkHttpClient.Builder()
                 .connectTimeout(API.CONNECT_TIMEOUT, TimeUnit.SECONDS)
