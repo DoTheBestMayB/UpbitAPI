45e6284b531765a135f79fe0f0bcc66dbac81e34
DoDoBest <av449@naver.com>
2022-03-23 14:46:16 +0900

    feat: data 모듈에 repository 인터페이스 구현 클래스 작성

---
+++ data/src/main/java/com/github/dodobest/data/UpbitRepositoryImpl.kt
@@ -0,0 +1,33 @@
+package com.github.dodobest.data
+
+import com.github.dodobest.domain.UpbitMarketData
+import com.github.dodobest.domain.UpbitRepository
+import com.github.dodobest.domain.UpbitTickerData
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Retrofit
+
+internal class UpbitRepositoryImpl(
+    private val retrofit: Retrofit
+) : UpbitRepository {
+    override fun getMarkets(
+        onSuccess: (List<UpbitMarketData>) -> Unit,
+        onFailure: (Throwable) -> Unit
+    ) {
+        val singleMarketData = retrofit.create(UpbitApiService::class.java).getMarkets()
+
+        singleMarketData.subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.computation())
+            .doOnSuccess { onSuccess(it.map { it.toData() }) }
+            .doOnError { onFailure(it) }
+    }
+
+    override fun getTicker(coinName: String, onSuccess: (UpbitTickerData) -> Unit, onFailure: (Throwable) -> Unit) {
+        val singleTickerData = retrofit.create(UpbitApiService::class.java).getTicker(coinName)
+
+        singleTickerData.subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.computation())
+            .doOnSuccess { onSuccess(it.toData()) }
+            .doOnError { onFailure(it) }
+    }
+
+}
