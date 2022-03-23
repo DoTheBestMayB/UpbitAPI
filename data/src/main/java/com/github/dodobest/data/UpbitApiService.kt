b32961e6f9f2416bdbb30ac13159e8db90193590
DoDoBest <av449@naver.com>
2022-03-23 14:44:47 +0900

    feat: retrofit api service 클래스에서 rxJava single 타입 사용

--- data/src/main/java/com/github/dodobest/data/UpbitApiService.kt
+++ data/src/main/java/com/github/dodobest/data/UpbitApiService.kt
@@ -1,14 +1,14 @@
 package com.github.dodobest.data
 
-import retrofit2.Response
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface UpbitApiService {
     @GET(API.MARKET_COIN_LIST_URL)
-    fun getMarkets(@Query("isDetails") isDetails: Boolean = true): Response<List<UpbitMarketResponse>>
+    fun getMarkets(@Query("isDetails") isDetails: Boolean = true): Single<List<UpbitMarketDataResponse>>
 
     @GET("${API.TICKER_URL}/{coinName}")
-    fun getTicker(@Path(value = "coinName", encoded = true) coinName: String) : Response<List<UpbitTickerResponse>>
+    fun getTicker(@Path(value = "coinName", encoded = true) coinName: String) : Single<UpbitTickerDataResponse>
 }
