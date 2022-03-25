efa4994962db19fab60e64585b941ce6d0f6f68f
DoDoBest <av449@naver.com>
2022-03-25 16:31:16 +0900

    fix: RemoteUpbitDataSource Retrofit parameter default value 구현

--- data/src/main/java/com/github/dodobest/data/RemoteUpbitDataSource.kt
+++ data/src/main/java/com/github/dodobest/data/RemoteUpbitDataSource.kt
@@ -1,10 +1,22 @@
 package com.github.dodobest.data
 
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import io.reactivex.rxjava3.schedulers.Schedulers
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 internal class RemoteUpbitDataSource(
-    private val retrofit: Retrofit
+    private val retrofit: Retrofit = Retrofit.Builder().baseUrl(API.BASE_URL)
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(OkHttpClient.Builder()
+            .connectTimeout(API.CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(API.READ_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(API.WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .build())
+        .build()
 ) {
     fun getMarkets(
         onSuccess: (List<UpbitMarketData>) -> Unit,
