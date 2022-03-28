1723e6dfbb72bf7ef1320a39e8383a4880629c09
DoDoBest <av449@naver.com>
2022-03-28 14:38:29 +0900

    refactor: Constants.kt에서 주석 삭제, 이름 변경

--- data/src/main/java/com/github/dodobest/data/Constants.kt
+++ data/src/main/java/com/github/dodobest/data/Constants.kt
@@ -1,9 +1,7 @@
 package com.github.dodobest.data
 
-object API {
+object OkHttpClientConstant {
     const val BASE_URL : String = "https://api.upbit.com/v1/"
-
-    // OkHttpClient
     const val CONNECT_TIMEOUT: Long = 3L
     const val READ_TIMEOUT: Long = 3L
     const val WRITE_TIMEOUT: Long = 3L
