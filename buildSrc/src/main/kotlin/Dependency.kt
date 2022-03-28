3f754d04ed5486a34b3f137092837d32df30293c
DoDoBest <av449@naver.com>
2022-03-28 10:44:05 +0900

    refactor: Versions.kt 지우고 Dependency.kt 로 변수 옮기기

--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -1,35 +1,57 @@
 object BuildPlugins {
-    const val ANDROID = "com.android.tools.build:gradle:${Version.GRADLE_PLUGIN}"
-    const val KOTLIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.KOTLIN}"
+    private const val GRADLE_PLUGIN_VERSION = "7.1.2"
+    private const val KOTLIN_VERSION = "1.5.21"
+
+    const val ANDROID = "com.android.tools.build:gradle:${GRADLE_PLUGIN_VERSION}"
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${KOTLIN_VERSION}"
 }
 
 object Androidx {
-    const val CORE_KTX = "androidx.core:core-ktx:${Version.CORE_KTX}"
-    const val APP_COMPAT = "androidx.appcompat:appcompat:${Version.APP_COMPAT}"
-    const val MATERIAL_DESIGN = "com.google.android.material:material:${Version.MATERIAL}"
-    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:${Version.CONSTRAINT_LAYOUT}"
-    const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Version.FRAGMENT_KTX}"
+    private const val CORE_KTX_VERSION = "1.7.0"
+    private const val APP_COMPAT_VERSION = "1.4.1"
+    private const val MATERIAL_VERSION = "1.5.0"
+    private const val CONSTRAINT_LAYOUT_VERSION = "2.1.3"
+    private const val FRAGMENT_KTX_VERSION = "1.3.0"
+
+    const val CORE_KTX = "androidx.core:core-ktx:${CORE_KTX_VERSION}"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:${APP_COMPAT_VERSION}"
+    const val MATERIAL_DESIGN = "com.google.android.material:material:${MATERIAL_VERSION}"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:${CONSTRAINT_LAYOUT_VERSION}"
+    const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${FRAGMENT_KTX_VERSION}"
 }
 
 object Network {
-    const val RETROFIT = "com.squareup.retrofit2:retrofit:${Version.RETROFIT}"
-    const val GSON = "com.squareup.retrofit2:converter-gson:${Version.GSON}"
-    const val CONVERTER_GSON = "com.squareup.retrofit2:converter-gson:${Version.GSON}"
-    const val OKHTTP3 = "com.squareup.okhttp3:okhttp:${Version.OKHTTP3}"
+    private const val RETROFIT_VERSION = "2.9.0"
+    private const val GSON_VERSION = "2.9.0"
+    private const val OKHTTP3_VERSION = "4.9.3"
 
-    const val MOCK_WEB_SERVER = "com.squareup.okhttp3:mockwebserver:${Version.OKHTTP3}"
+    const val RETROFIT = "com.squareup.retrofit2:retrofit:${RETROFIT_VERSION}"
+    const val GSON = "com.squareup.retrofit2:converter-gson:${GSON_VERSION}"
+    const val CONVERTER_GSON = "com.squareup.retrofit2:converter-gson:${GSON_VERSION}"
+    const val OKHTTP3 = "com.squareup.okhttp3:okhttp:${OKHTTP3_VERSION}"
+
+    const val MOCK_WEB_SERVER = "com.squareup.okhttp3:mockwebserver:${OKHTTP3_VERSION}"
 }
 
 object RxJava {
-    const val RXJAVA3 = "io.reactivex.rxjava3:rxjava:${Version.RXJAVA3}"
-    const val RXJAVA3_RETROFIT_ADAPTER = "com.github.akarnokd:rxjava3-retrofit-adapter:${Version.RXJAVA3_RETROFIT_ADAPTER}"
+    private const val RXJAVA3_VERSION = "3.1.3"
+    private const val RXJAVA3_RETROFIT_ADAPTER_VERSION = "3.0.0"
+
+    const val RXJAVA3 = "io.reactivex.rxjava3:rxjava:${RXJAVA3_VERSION}"
+    const val RXJAVA3_RETROFIT_ADAPTER = "com.github.akarnokd:rxjava3-retrofit-adapter:${RXJAVA3_RETROFIT_ADAPTER_VERSION}"
 }
 
 object Test {
-    const val JUNIT = "junit:junit:${Version.JUNIT}"
-    const val TRUTH = "com.google.truth:truth:${Version.TRUTH}"
-    const val TEST_TRUTH = "androidx.test.ext:truth:${Version.TEST_TRUTH}"
+    private const val JUNIT_VERSION = "4.13.2"
+    private const val ANDROIDX_JUNIT_VERSION = "1.1.3"
+    private const val ESPRESSO_CORE_VERSION = "3.4.0"
+    private const val TRUTH_VERSION = "1.1.3"
+    private const val TEST_TRUTH_VERSION = "1.4.0"
 
-    const val ANDROIDX_JUNIT = "androidx.test.ext:junit:${Version.ANDROIDX_JUNIT}"
-    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Version.ESPRESSO_CORE}"
+    const val JUNIT = "junit:junit:${JUNIT_VERSION}"
+    const val TRUTH = "com.google.truth:truth:${TRUTH_VERSION}"
+    const val TEST_TRUTH = "androidx.test.ext:truth:${TEST_TRUTH_VERSION}"
+
+    const val ANDROIDX_JUNIT = "androidx.test.ext:junit:${ANDROIDX_JUNIT_VERSION}"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${ESPRESSO_CORE_VERSION}"
 }
