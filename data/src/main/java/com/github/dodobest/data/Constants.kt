84b2c4e11cbc038e5cc5815239549b912ea667d8
DoDoBest <av449@naver.com>
2022-03-28 14:29:46 +0900

    refactor: UpbitAPI에서 사용하는 상수 companion object에 위치시키기

--- data/src/main/java/com/github/dodobest/data/Constants.kt
+++ data/src/main/java/com/github/dodobest/data/Constants.kt
@@ -2,8 +2,6 @@ package com.github.dodobest.data
 
 object API {
     const val BASE_URL : String = "https://api.upbit.com/v1/"
-    const val TICKER_URL : String = "ticker"
-    const val MARKET_COIN_LIST_URL : String = "market/all"
 
     // OkHttpClient
     const val CONNECT_TIMEOUT: Long = 3L
--- data/src/main/java/com/github/dodobest/data/UpbitAPI.kt
+++ data/src/main/java/com/github/dodobest/data/UpbitAPI.kt
@@ -5,9 +5,14 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface UpbitAPI {
-    @GET(API.MARKET_COIN_LIST_URL)
+    @GET(MARKET_COIN_LIST_URL)
     fun getMarkets(@Query("isDetails") isDetails: Boolean = true): Single<List<UpbitMarketData>>
 
-    @GET(API.TICKER_URL)
+    @GET(TICKER_URL)
     fun getTicker(@Query(value = "markets") coinName: String): Single<List<UpbitTickerData>>
+
+    companion object {
+        const val MARKET_COIN_LIST_URL : String = "market/all"
+        const val TICKER_URL : String = "ticker"
+    }
 }
