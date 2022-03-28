0c5dae7f26ececf6604e0329a4fb3572cea3ee08
DoDoBest <av449@naver.com>
2022-03-28 14:40:41 +0900

    refactor: BASE_URl 상수 UpbitClient 클래스로 이동

--- data/src/main/java/com/github/dodobest/data/Constants.kt
+++ data/src/main/java/com/github/dodobest/data/Constants.kt
@@ -1,7 +1,6 @@
 package com.github.dodobest.data
 
 object OkHttpClientConstant {
-    const val BASE_URL : String = "https://api.upbit.com/v1/"
     const val CONNECT_TIMEOUT: Long = 3L
     const val READ_TIMEOUT: Long = 3L
     const val WRITE_TIMEOUT: Long = 3L
--- data/src/main/java/com/github/dodobest/data/UpbitClient.kt
+++ data/src/main/java/com/github/dodobest/data/UpbitClient.kt
@@ -1,5 +1,7 @@
 package com.github.dodobest.data
 
 internal class UpbitClient {
-
+    companion object {
+        const val BASE_URL : String = "https://api.upbit.com/v1/"
+    }
 }
