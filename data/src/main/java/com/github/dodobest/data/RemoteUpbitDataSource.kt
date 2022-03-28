b0f4e6166556cecebfb3f8b915a4d3ff73d0fef9
DoDoBest <av449@naver.com>
2022-03-28 10:58:58 +0900

    refactor: 데이터소스에서 서비스 인터페이스를 생성자로 주입

--- data/src/main/java/com/github/dodobest/data/RemoteUpbitDataSource.kt
+++ data/src/main/java/com/github/dodobest/data/RemoteUpbitDataSource.kt
@@ -8,21 +8,13 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 internal class RemoteUpbitDataSource(
-    private val retrofit: Retrofit = Retrofit.Builder().baseUrl(API.BASE_URL)
-        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(OkHttpClient.Builder()
-            .connectTimeout(API.CONNECT_TIMEOUT, TimeUnit.SECONDS)
-            .readTimeout(API.READ_TIMEOUT, TimeUnit.SECONDS)
-            .writeTimeout(API.WRITE_TIMEOUT, TimeUnit.SECONDS)
-            .build())
-        .build()
+    private val upbitApiService: UpbitApiService
 ) {
     fun getMarkets(
         onSuccess: (List<UpbitMarketData>) -> Unit,
         onFailure: (Throwable) -> Unit
     ) {
-        val singleMarketData = retrofit.create(UpbitApiService::class.java).getMarkets()
+        val singleMarketData = upbitApiService.getMarkets()
 
         singleMarketData.subscribeOn(Schedulers.io())
             .observeOn(Schedulers.computation())
@@ -34,7 +26,7 @@ internal class RemoteUpbitDataSource(
     }
 
     fun getTicker(coinName: String, onSuccess: (List<UpbitTickerData>) -> Unit, onFailure: (Throwable) -> Unit) {
-        val singleTickerData = retrofit.create(UpbitApiService::class.java).getTicker(coinName)
+        val singleTickerData = upbitApiService.getTicker(coinName)
 
         singleTickerData.subscribeOn(Schedulers.io())
             .observeOn(Schedulers.computation())
