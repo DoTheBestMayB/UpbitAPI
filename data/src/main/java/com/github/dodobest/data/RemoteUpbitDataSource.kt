24c6c3a3f9875b376435d6ed38f23164ea7502ac
DoDoBest <av449@naver.com>
2022-03-28 13:51:39 +0900

    refactor: DataSource는 DataResponse 만 반환하도록 수정

--- data/src/main/java/com/github/dodobest/data/RemoteUpbitDataSource.kt
+++ data/src/main/java/com/github/dodobest/data/RemoteUpbitDataSource.kt
@@ -1,6 +1,7 @@
 package com.github.dodobest.data
 
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -10,31 +11,12 @@ import java.util.concurrent.TimeUnit
 internal class RemoteUpbitDataSource(
     private val upbitApiService: UpbitApiService
 ) {
-    fun getMarkets(
-        onSuccess: (List<UpbitMarketData>) -> Unit,
-        onFailure: (Throwable) -> Unit
-    ) {
-        val singleMarketData = upbitApiService.getMarkets()
-
-        singleMarketData.subscribeOn(Schedulers.io())
-            .observeOn(Schedulers.computation())
-            .subscribe({
-                onSuccess(it)
-            }, {
-                onFailure(it)
-            })
+    fun getMarkets(): Single<List<UpbitMarketData>> {
+        return upbitApiService.getMarkets()
     }
 
-    fun getTicker(coinName: String, onSuccess: (List<UpbitTickerData>) -> Unit, onFailure: (Throwable) -> Unit) {
-        val singleTickerData = upbitApiService.getTicker(coinName)
-
-        singleTickerData.subscribeOn(Schedulers.io())
-            .observeOn(Schedulers.computation())
-            .subscribe({
-                onSuccess(it)
-            }, {
-                onFailure(it)
-            })
+    fun getTicker(coinName: String): Single<List<UpbitTickerData>> {
+        return upbitApiService.getTicker(coinName)
     }
 
 }
