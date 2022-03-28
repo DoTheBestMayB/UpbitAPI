be21245452630deae5dfdfd2955cbc1da9406fcd
DoDoBest <av449@naver.com>
2022-03-28 15:09:53 +0900

    refactor: 불필요한 상수 삭제

--- data/src/main/java/com/github/dodobest/data/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.github.dodobest.data
-
-object OkHttpClientConstant {
-    const val CONNECT_TIMEOUT: Long = 3L
-    const val READ_TIMEOUT: Long = 3L
-    const val WRITE_TIMEOUT: Long = 3L
-}
