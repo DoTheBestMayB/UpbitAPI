aefdecf040cde5473aaee1918dc785a701028000
DoDoBest <av449@naver.com>
2022-03-30 20:46:26 +0900

    refactor: 사용하지 않는 변수 삭제

--- data/src/test/kotlin/com/github/dodobest/data/model/UpbitRemoteDataSourceImplTest.kt
+++ data/src/test/kotlin/com/github/dodobest/data/model/UpbitRemoteDataSourceImplTest.kt
@@ -5,22 +5,17 @@ import com.github.dodobest.data.remote.UpbitRemoteDataSource
 import com.github.dodobest.data.remote.UpbitRemoteDataSourceImpl
 import io.mockk.mockk
 import io.mockk.verify
-import io.reactivex.rxjava3.core.Single
 import org.junit.Before
 import org.junit.Test
 
 internal class UpbitRemoteDataSourceImplTest {
     private lateinit var upbitAPI: UpbitAPI
     private lateinit var upbitRemoteDataSource: UpbitRemoteDataSource
-    private lateinit var upbitTickerDataMockk: Single<List<UpbitTickerData>>
-    private lateinit var upbitMarketDataMockk: Single<List<UpbitMarketData>>
 
     @Before
     fun setUp() {
         upbitAPI = mockk(relaxed=true)
         upbitRemoteDataSource = UpbitRemoteDataSourceImpl(upbitAPI)
-        upbitTickerDataMockk = mockk()
-        upbitMarketDataMockk = mockk()
     }
 
     @Test
