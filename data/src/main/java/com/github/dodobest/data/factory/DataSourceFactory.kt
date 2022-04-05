32d20c33e8b1e65ba02e8081050242da3a0e0360
DoDoBest <av449@naver.com>
2022-04-05 16:20:21 +0900

    refactor: DataSourceFactory 함수 Retrofit 주입 받도록 수정

--- data/src/main/java/com/github/dodobest/data/factory/DataSourceFactory.kt
+++ data/src/main/java/com/github/dodobest/data/factory/DataSourceFactory.kt
@@ -2,11 +2,12 @@ package com.github.dodobest.data.factory
 
 import com.github.dodobest.data.remote.UpbitAPI
 import com.github.dodobest.data.remote.UpbitRemoteDataSourceImpl
+import retrofit2.Retrofit
 
 internal interface DataSourceFactory {
     companion object {
-        fun newUpbitDataSource() : UpbitRemoteDataSourceImpl {
-            return UpbitRemoteDataSourceImpl(RetrofitClient.getUpbitRetrofit().create(UpbitAPI::class.java))
+        fun newUpbitDataSource(retrofit: Retrofit) : UpbitRemoteDataSourceImpl {
+            return UpbitRemoteDataSourceImpl(retrofit.create(UpbitAPI::class.java))
         }
     }
 }
