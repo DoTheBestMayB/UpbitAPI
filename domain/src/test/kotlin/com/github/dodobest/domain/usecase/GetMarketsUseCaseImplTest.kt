6c54e20e8ad4e76f2891f4d609de78c4694759e9
DoDoBest <av449@naver.com>
2022-04-07 21:58:32 +0900

    test: usecase 테스트 함수 작성

---
+++ domain/src/test/kotlin/com/github/dodobest/domain/usecase/GetMarketsUseCaseImplTest.kt
@@ -0,0 +1,28 @@
+package com.github.dodobest.domain.usecase
+
+import com.github.dodobest.domain.UpbitRepository
+import io.mockk.mockk
+import io.mockk.verify
+
+import org.junit.Before
+import org.junit.Test
+
+class GetMarketsUseCaseImplTest {
+    lateinit var upbitRepository: UpbitRepository
+    lateinit var getMarketsUseCase: GetMarketsUseCase
+
+    @Before
+    fun setUp() {
+        upbitRepository = mockk(relaxed = true)
+        getMarketsUseCase = GetMarketsUseCaseImpl(upbitRepository)
+    }
+
+    @Test
+    fun `getMarketUseCase()를 호출하면 upbitRepository의 getMarkets()를 호출한다`() {
+        // when
+        getMarketsUseCase()
+
+        // then
+        verify { upbitRepository.getMarkets() }
+    }
+}
---
+++ domain/src/test/kotlin/com/github/dodobest/domain/usecase/GetTickerUseCaseImplTest.kt
@@ -0,0 +1,28 @@
+package com.github.dodobest.domain.usecase
+
+import com.github.dodobest.domain.UpbitRepository
+import io.mockk.mockk
+import io.mockk.verify
+
+import org.junit.Before
+import org.junit.Test
+
+class GetTickerUseCaseImplTest {
+    lateinit var upbitRepository: UpbitRepository
+    lateinit var getTickerUseCase: GetTickerUseCase
+
+    @Before
+    fun setUp() {
+        upbitRepository = mockk(relaxed = true)
+        getTickerUseCase = GetTickerUseCaseImpl(upbitRepository)
+    }
+
+    @Test
+    fun `getTickerUseCase()를 호출하면 upbitRepository의 getTicker()를 호출한다`() {
+        // when
+        getTickerUseCase("KRW-BTC")
+
+        // then
+        verify { upbitRepository.getTicker("KRW-BTC") }
+    }
+}
