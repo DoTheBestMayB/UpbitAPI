968949915a83cff1cf52c1de294213d07f1532f4
DoDoBest <av449@naver.com>
2022-04-07 23:32:16 +0900

    feat: usecase 펙토리 함수 추가

---
+++ domain/src/main/java/com/github/dodobest/domain/factory/GetMarketsUseCaseFactory.kt
@@ -0,0 +1,19 @@
+package com.github.dodobest.domain.factory
+
+import com.github.dodobest.domain.UpbitRepository
+import com.github.dodobest.domain.usecase.GetMarketsUseCase
+import com.github.dodobest.domain.usecase.GetMarketsUseCaseImpl
+
+object GetMarketsUseCaseFactory {
+    private lateinit var getMarketsUseCase: GetMarketsUseCase
+
+    fun getGetMarketsUseCase(upbitRepository: UpbitRepository) : GetMarketsUseCase {
+        if (::getMarketsUseCase.isInitialized) {
+            return getMarketsUseCase
+        }
+
+        getMarketsUseCase = GetMarketsUseCaseImpl(upbitRepository)
+
+        return getMarketsUseCase
+    }
+}
