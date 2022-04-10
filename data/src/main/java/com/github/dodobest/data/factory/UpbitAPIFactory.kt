f9cb425019ce728db112007a26edb806ac8b215e
DoDoBest <av449@naver.com>
2022-04-10 13:41:48 +0900

    chore: UpbitAPIFactory 함수가 아닌 클래스를 internal로 선언

--- data/src/main/java/com/github/dodobest/data/factory/UpbitAPIFactory.kt
+++ data/src/main/java/com/github/dodobest/data/factory/UpbitAPIFactory.kt
@@ -3,10 +3,10 @@ package com.github.dodobest.data.factory
 import com.github.dodobest.data.remote.UpbitAPI
 import retrofit2.Retrofit
 
-object UpbitAPIFactory {
+internal object UpbitAPIFactory {
     private lateinit var upbitAPI: UpbitAPI
 
-    internal fun getUpbitAPI(retrofit: Retrofit): UpbitAPI {
+    fun getUpbitAPI(retrofit: Retrofit): UpbitAPI {
         if (::upbitAPI.isInitialized) {
             return upbitAPI
         }
