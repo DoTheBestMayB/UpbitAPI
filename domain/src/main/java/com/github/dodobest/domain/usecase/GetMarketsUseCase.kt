85cbbcbbdfd4cb110c15a20a95720f35544d28b1
DoDoBest <av449@naver.com>
2022-04-11 01:54:31 +0900

    refactor: companion object factory function 구현

--- domain/src/main/java/com/github/dodobest/domain/usecase/GetMarketsUseCase.kt
+++ domain/src/main/java/com/github/dodobest/domain/usecase/GetMarketsUseCase.kt
@@ -1,8 +1,15 @@
 package com.github.dodobest.domain.usecase
 
+import com.github.dodobest.domain.UpbitRepository
 import com.github.dodobest.domain.model.UpbitMarketData
 import io.reactivex.rxjava3.core.Single
 
 interface GetMarketsUseCase {
     operator fun invoke(): Single<List<UpbitMarketData>>
+
+    companion object {
+        fun of(upbitRepository: UpbitRepository): GetMarketsUseCase {
+            return GetMarketsUseCaseImpl(upbitRepository)
+        }
+    }
 }
--- domain/src/main/java/com/github/dodobest/domain/usecase/GetTickerUseCase.kt
+++ domain/src/main/java/com/github/dodobest/domain/usecase/GetTickerUseCase.kt
@@ -1,8 +1,15 @@
 package com.github.dodobest.domain.usecase
 
+import com.github.dodobest.domain.UpbitRepository
 import com.github.dodobest.domain.model.UpbitTickerData
 import io.reactivex.rxjava3.core.Single
 
 interface GetTickerUseCase {
     operator fun invoke(coinName: String): Single<List<UpbitTickerData>>
+
+    companion object {
+        fun of(upbitRepository: UpbitRepository): GetTickerUseCase {
+            return GetTickerUseCaseImpl(upbitRepository)
+        }
+    }
 }
