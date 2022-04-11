1437e0a115f5498d6e27e0d3fc41af79590734f1
DoDoBest <av449@naver.com>
2022-04-11 21:57:14 +0900

    refactor: DataSourceFactory 구현체 대신 인터페이스 리턴

--- data/src/main/java/com/github/dodobest/data/factory/DataSourceFactory.kt
+++ data/src/main/java/com/github/dodobest/data/factory/DataSourceFactory.kt
@@ -1,12 +1,13 @@
 package com.github.dodobest.data.factory
 
 import com.github.dodobest.data.remote.UpbitAPI
+import com.github.dodobest.data.remote.UpbitRemoteDataSource
 import com.github.dodobest.data.remote.UpbitRemoteDataSourceImpl
 import retrofit2.Retrofit
 
 internal interface DataSourceFactory {
     companion object {
-        fun newUpbitDataSource(retrofit: Retrofit): UpbitRemoteDataSourceImpl {
+        fun newUpbitDataSource(retrofit: Retrofit): UpbitRemoteDataSource {
             return UpbitRemoteDataSourceImpl(retrofit.create(UpbitAPI::class.java))
         }
     }
