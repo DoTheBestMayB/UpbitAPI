b8ee52144ae511ab711468ce719b19af5502bb79
DoDoBest <av449@naver.com>
2022-04-12 20:45:25 +0900

    refactor: SingletonHolder 불필요한 변수 삭제

--- data/src/main/java/com/github/dodobest/data/factory/SingletonHolder.kt
+++ data/src/main/java/com/github/dodobest/data/factory/SingletonHolder.kt
@@ -1,26 +1,19 @@
 package com.github.dodobest.data.factory
 
-open class SingletonHolder<out T, in A>(creator: (A) -> (T)) {
-    private var creator: ((A) -> (T))? = creator
+open class SingletonHolder<out T, in A>(val creator: (A) -> (T)) {
     @Volatile
     private var instance: T? = null
 
     fun getInstance(arg: A): T {
-        val i = instance
-        if (i != null) {
-            return i
+        if (instance != null) {
+            return instance!!
         }
 
         return synchronized(this) {
-            val i2 = instance
-            if (i2 != null) {
-                i2
-            } else {
-                val created = creator!!(arg)
-                instance = created
-                creator = null
-                created
+            if (instance == null) {
+                instance = creator(arg)
             }
+            instance!!
         }
     }
 }
