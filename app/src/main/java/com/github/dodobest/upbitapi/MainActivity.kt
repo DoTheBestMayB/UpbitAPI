6a6ff05e17b83613d939c00f7c41184eda000310
DoDoBest <av449@naver.com>
2022-04-15 23:41:24 +0900

    feat: MainActivity 데이터 바인딩 적용

--- app/src/main/java/com/github/dodobest/upbitapi/MainActivity.kt
+++ app/src/main/java/com/github/dodobest/upbitapi/MainActivity.kt
@@ -2,10 +2,14 @@ package com.github.dodobest.upbitapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.dodobest.upbitapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
