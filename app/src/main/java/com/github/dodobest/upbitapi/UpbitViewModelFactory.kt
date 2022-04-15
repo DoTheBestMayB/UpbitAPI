9ef0e995a8a569786351d9e0b768ab41542347c4
DoDoBest <av449@naver.com>
2022-04-16 00:20:01 +0900

    feat: viewModelFactory 구현

---
+++ app/src/main/java/com/github/dodobest/upbitapi/UpbitViewModelFactory.kt
@@ -0,0 +1,26 @@
+package com.github.dodobest.upbitapi
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.github.dodobest.data.Injector
+
+class UpbitViewModelFactory : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return when (modelClass) {
+            UpbitViewModel::class.java -> createViewModel()
+            else -> throw IllegalArgumentException()
+        } as T
+    }
+
+    private fun createViewModel(): UpbitViewModel {
+        val upbitRetrofit = Injector.provideUpbitRetrofit()
+        val upbitAPI = Injector.provideUpbitAPI(upbitRetrofit)
+        val upbitRemoteDataSource = Injector.provideUpbitRemoteDataSource(upbitAPI)
+        val upbitRepository = Injector.provideUpbitRepository(upbitRemoteDataSource)
+        val getMarketsUseCase = Injector.provideGetMarketsUseCase(upbitRepository)
+        val getTickerUseCase = Injector.provideGetTickerUseCase(upbitRepository)
+
+        return UpbitViewModel(getMarketsUseCase, getTickerUseCase)
+    }
+}
