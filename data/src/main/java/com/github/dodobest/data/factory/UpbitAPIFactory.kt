9d3b5c4ef1185ffa184f79e3614398805ae22a76
DoDoBest <av449@naver.com>
2022-04-16 00:29:49 +0900

    refactor: UpbitAPIFactory 사용하지 않는 생성자 삭제

--- data/src/main/java/com/github/dodobest/data/factory/UpbitAPIFactory.kt
+++ data/src/main/java/com/github/dodobest/data/factory/UpbitAPIFactory.kt
@@ -3,7 +3,7 @@ package com.github.dodobest.data.factory
 import com.github.dodobest.data.remote.UpbitAPI
 import retrofit2.Retrofit
 
-internal class UpbitAPIFactory private constructor(retrofit: Retrofit) {
+internal class UpbitAPIFactory {
     companion object : SingletonHolder<UpbitAPI, Retrofit>({
         it.create(UpbitAPI::class.java)
     })
