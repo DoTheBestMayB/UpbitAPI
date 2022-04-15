9b8e9e6b42d37708140a97b20ab49de1d48adef2
DoDoBest <av449@naver.com>
2022-04-16 00:21:10 +0900

    feat: MainActivity에서 viewModel 변수 생성

--- app/src/main/java/com/github/dodobest/upbitapi/MainActivity.kt
+++ app/src/main/java/com/github/dodobest/upbitapi/MainActivity.kt
@@ -2,10 +2,12 @@ package com.github.dodobest.upbitapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.github.dodobest.upbitapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private val viewModel: UpbitViewModel by viewModels { UpbitViewModelFactory() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
