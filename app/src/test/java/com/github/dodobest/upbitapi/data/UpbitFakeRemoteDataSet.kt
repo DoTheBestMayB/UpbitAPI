44dc61fc204e2e7f0d90c2051a04124efedf30a1
DoDoBest <av449@naver.com>
2022-04-18 01:49:11 +0900

    test: FakeRemoteDataSource 만들기

---
+++ app/src/test/java/com/github/dodobest/upbitapi/data/UpbitFakeRemoteDataSet.kt
@@ -0,0 +1,59 @@
+package com.github.dodobest.upbitapi.data
+
+import com.github.dodobest.data.model.UpbitMarketData
+import com.github.dodobest.data.model.UpbitTickerData
+
+object UpbitFakeRemoteDataSet {
+    val upbitMarketData = listOf(
+        UpbitMarketData(
+            "KRW-BTC",
+            "비트코인",
+            "Bitcoin",
+            "None",
+        ),
+        UpbitMarketData(
+            "KRW-ETH",
+            "이더리움",
+            "Ethereum",
+            "NONE",
+        ),
+        UpbitMarketData(
+            "KRW-NU",
+            "누사이퍼",
+            "Nucypher",
+            "CAUTION",
+        )
+    )
+
+    val upbitBTCTickerData = listOf(
+        UpbitTickerData(
+            "KRW-BTC",
+            8450000.0,
+            8621000.0,
+            171000.0,
+            232702901371.09308,
+        )
+    )
+
+    val upbitETHTickerData = listOf(
+        UpbitTickerData(
+            "KRW-ETH",
+            1000.0,
+            1500.0,
+            500.0,
+            6000.0,
+        )
+    )
+
+    val upbitNUTickerData = listOf(
+        UpbitTickerData(
+            "KRW-NU",
+            10.0,
+            5.0,
+            -5.0,
+            20000.0,
+        )
+    )
+
+    val upbitTickerFailData = listOf<UpbitTickerData>()
+}
