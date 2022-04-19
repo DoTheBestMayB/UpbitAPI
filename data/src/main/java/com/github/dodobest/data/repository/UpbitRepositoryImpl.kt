6abcb816572cca15a02d92aae43612de8937f1a8
DoDoBest <av449@naver.com>
2022-04-19 10:04:24 +0900

    refactor: Repository에서 Single 데이터 subscribeOn 설정

--- data/src/main/java/com/github/dodobest/data/repository/UpbitRepositoryImpl.kt
+++ data/src/main/java/com/github/dodobest/data/repository/UpbitRepositoryImpl.kt
@@ -6,6 +6,7 @@ import com.github.dodobest.domain.UpbitRepository
 import com.github.dodobest.domain.model.UpbitMarketData
 import com.github.dodobest.domain.model.UpbitTickerData
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 internal class UpbitRepositoryImpl(
     private val upbitRemoteDataSource: UpbitRemoteDataSource
@@ -15,7 +16,7 @@ internal class UpbitRepositoryImpl(
             upbitMarketDataList.map { upbitMarketData ->
                 upbitMarketData.toDomainData()
             }
-        }
+        }.subscribeOn(Schedulers.io())
     }
 
     override fun getTicker(market: String): Single<List<UpbitTickerData>> {
@@ -23,7 +24,7 @@ internal class UpbitRepositoryImpl(
             upbitTickerDataList.map { upbitTickerData ->
                 upbitTickerData.toDomainData()
             }
-        }
+        }.subscribeOn(Schedulers.io())
     }
 
     companion object :
