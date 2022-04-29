058c241423e72846562328ed4b6912b9b281446f
DoDoBest <av449@naver.com>
2022-04-29 14:15:54 +0900

    refactor: repository Hilt provides 함수 Singleton 적용하기

--- data/src/main/java/com/github/dodobest/data/di/UpbitRepositoryModule.kt
+++ data/src/main/java/com/github/dodobest/data/di/UpbitRepositoryModule.kt
@@ -6,11 +6,13 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface UpbitRepositoryModule {
     @Binds
+    @Singleton
     fun provideUpbitRepository(
         upbitRepositoryImpl: UpbitRepositoryImpl
     ): UpbitRepository
