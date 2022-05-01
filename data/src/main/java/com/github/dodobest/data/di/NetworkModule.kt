4f44f1839b0135d7f0d1b6b5b63435d4c6b62843
DoDoBest <av449@naver.com>
2022-05-01 23:48:37 +0900

    refactor: 네트워크 관련 Hilt 클래스 NetworkModule 클래스에서 관리

--- data/src/main/java/com/github/dodobest/data/di/NetworkModule.kt
+++ data/src/main/java/com/github/dodobest/data/di/NetworkModule.kt
@@ -5,18 +5,35 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import okhttp3.OkHttpClient
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal object OkHttpClientModule {
+internal object NetworkModule {
     @Provides
+    @Singleton
+    fun provideGsonConverter(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
     fun provideOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
             .connectTimeout(Constant.CONNECT_TIMEOUT, TimeUnit.SECONDS)
             .readTimeout(Constant.READ_TIMEOUT, TimeUnit.SECONDS)
             .writeTimeout(Constant.WRITE_TIMEOUT, TimeUnit.SECONDS)
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRxJava3CallAdapter(): RxJava3CallAdapterFactory {
+        return RxJava3CallAdapterFactory.create()
     }
 }
