c96cff6d4ad6d57bccd74740615da4c9846eb676
DoDoBest <av449@naver.com>
2022-05-01 22:21:49 +0900

    refactor: UseCase Hilt 코드, 한 클래스에서 관리

--- domain/src/main/java/com/github/dodobest/domain/di/UpbitUseCaseModule.kt
+++ domain/src/main/java/com/github/dodobest/domain/di/UpbitUseCaseModule.kt
@@ -1,5 +1,7 @@
 package com.github.dodobest.domain.di
 
+import com.github.dodobest.domain.usecase.GetMarketsUseCase
+import com.github.dodobest.domain.usecase.GetMarketsUseCaseImpl
 import com.github.dodobest.domain.usecase.GetTickerUseCase
 import com.github.dodobest.domain.usecase.GetTickerUseCaseImpl
 import dagger.Binds
@@ -9,9 +11,14 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal interface GetTickerUseCaseModule {
+internal interface UpbitUseCaseModule {
     @Binds
-    fun bindGetTickerUseCase(
+    fun provideGetMarketsUseCase(
+        getMarketsUseCaseImpl: GetMarketsUseCaseImpl
+    ) : GetMarketsUseCase
+
+    @Binds
+    fun provideGetTickerUseCase(
         getTickerUseCaseImpl: GetTickerUseCaseImpl
-    ): GetTickerUseCase
+    ) : GetTickerUseCase
 }
