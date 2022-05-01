f1ce017edd97bb3da0a3b52b4b2fceb5c0a28a67
DoDoBest <av449@naver.com>
2022-05-01 22:12:49 +0900

    refactor: OkHttpClient 객체 Hilt 주입

---
+++ data/src/main/java/com/github/dodobest/data/di/OkHttpClientModule.kt
@@ -0,0 +1,23 @@
+package com.github.dodobest.data.di
+
+import com.github.dodobest.data.Constant
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object OkHttpClientModule {
+
+    @Provides
+    fun provideOkHttpClient() : OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(Constant.CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(Constant.READ_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(Constant.WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .build()
+    }
+}
--- data/src/main/java/com/github/dodobest/data/di/UpbitRetrofitModule.kt
+++ data/src/main/java/com/github/dodobest/data/di/UpbitRetrofitModule.kt
@@ -1,6 +1,5 @@
 package com.github.dodobest.data.di
 
-import com.github.dodobest.data.Constant
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -9,7 +8,6 @@ import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -22,18 +20,13 @@ internal object UpbitRetrofitModule {
     fun provideUpbitRetrofit(
         rxJava3CallAdapterFactory: RxJava3CallAdapterFactory,
         gsonConverterFactory: GsonConverterFactory,
+        okHttpClient: OkHttpClient,
     ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(UPBIT_BASE_URL)
             .addCallAdapterFactory(rxJava3CallAdapterFactory)
             .addConverterFactory(gsonConverterFactory)
-            .client(
-                OkHttpClient.Builder()
-                    .connectTimeout(Constant.CONNECT_TIMEOUT, TimeUnit.SECONDS)
-                    .readTimeout(Constant.READ_TIMEOUT, TimeUnit.SECONDS)
-                    .writeTimeout(Constant.WRITE_TIMEOUT, TimeUnit.SECONDS)
-                    .build()
-            )
+            .client(okHttpClient)
             .build()
     }
 }
