ad72646c7fe1b88ef9a89fbf4c9ae0b9e4fc9e3d
DoDoBest <av449@naver.com>
2022-05-31 04:08:22 +0900

    refactor: ViewPagerAdapter의 fragment 생성 갯수 로직 변경

--- app/src/main/java/com/github/dodobest/upbitapi/CoinListViewPagerAdapter.kt
+++ app/src/main/java/com/github/dodobest/upbitapi/CoinListViewPagerAdapter.kt
@@ -3,18 +3,15 @@ package com.github.dodobest.upbitapi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.github.dodobest.upbitapi.model.MarketPlaceName
 
 class CoinListViewPagerAdapter(
-    fragmentActivity: FragmentActivity
+    fragmentActivity: FragmentActivity,
 ) : FragmentStateAdapter(fragmentActivity) {
 
-    override fun getItemCount(): Int = NUM_PAGES
+    override fun getItemCount(): Int = MarketPlaceName.values().size-1
 
     override fun createFragment(position: Int): Fragment {
         return CoinListPageFragment.newInstance(position)
-    }
-
-    companion object {
-        private const val NUM_PAGES = 3
     }
 }
