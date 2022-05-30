aa48bce43ed0d1af0e22dd7f8fe40972c611a009
DoDoBest <av449@naver.com>
2022-05-31 05:57:36 +0900

    chore: let 없이 코드 작성

--- app/src/main/java/com/github/dodobest/upbitapi/MainActivity.kt
+++ app/src/main/java/com/github/dodobest/upbitapi/MainActivity.kt
@@ -28,13 +28,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupUI() {
         binding?.let {
-            it.coinListViewPager.let { viewPager2 ->
-                viewPager2.adapter = CoinListViewPagerAdapter(this)
-
-                TabLayoutMediator(it.coinListTabLayout, viewPager2) { tab, position ->
-                    tab.text = MarketPlaceName.from(position).toString()
-                }.attach()
-            }
+            it.coinListViewPager.adapter = CoinListViewPagerAdapter(this)
+            TabLayoutMediator(it.coinListTabLayout, it.coinListViewPager) { tab, position ->
+                tab.text = MarketPlaceName.from(position).toString()
+            }.attach()
         } ?: throw IllegalArgumentException(getString(R.string.view_binding_is_null))
     }
 }
